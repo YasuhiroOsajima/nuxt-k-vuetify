153e1291468656e5a47a14fb64104ff8d8f6a0b9
Yasuhiro Osajima <kimukimu33@gmail.com>
2021-03-15 18:31:34 +0900

    remove portal-vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,6 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    'portal-vue/nuxt',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
